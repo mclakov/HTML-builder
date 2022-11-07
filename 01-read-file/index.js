021bac7c3bc505b8f61b22fddd2685d0f7296248
Roman Maklakov <mclakov@gmail.com>
2022-11-07 19:51:12 +0300

    feat: task 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const myPath = path.join(__dirname, 'secret-folder');
+const myPath = path.join(__dirname, './text.txt');
+const readableStream = fs.createReadStream(myPath, 'utf8');
+
+
+readableStream.on('data', function(chunk){
+	console.log(chunk);
+});

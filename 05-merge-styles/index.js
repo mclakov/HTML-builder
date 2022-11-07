1fea875535c3545c48b26d2b72596098be26c7fc
Roman Maklakov <mclakov@gmail.com>
2022-11-07 23:34:23 +0300

    fix: task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,19 +4,20 @@ const pathToStyle = path.join(__dirname, 'styles');
 const pathToDist = path.join(__dirname, 'project-dist');
 
 
-fs.readdir(pathToStyle, (err, data) => {
+fs.readdir(pathToDist, err2 => {
+	if (err2) throw err2;
+	fs.writeFile(path.join(pathToDist, 'bundle.css'), '', err1 => {
+		if (err1) throw err1;
+	});
+});
+
+fs.readdir(pathToStyle, (err, files) => {
 	if (err) throw err;
-	data.map(elem => {
+	files.map(elem => {
 		let pathToFile = path.join(pathToStyle, elem);
 		fs.stat(pathToFile, (err, stats) => {
 			if (err) throw err;
 			if (stats.isFile() && path.parse(pathToFile).ext === '.css') {
-				fs.readdir(pathToDist, err2 => {
-					if (err2) throw err2;
-					fs.writeFile(path.join(pathToDist, 'bundle.css'), '', err1 => {
-						if (err1) throw err1;
-					});
-				});
 				fs.readFile(pathToFile, 'utf8', (err, data) => {
 					if (err) throw err;
 					fs.readdir(pathToDist, err2 => {
